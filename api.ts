4037b1ce4b4da5511310f9a98826144095c60ef5
sunlee <sunlee128@gmail.com>
2020-06-10 21:48:11 +1000

    add scv, model

--- api.ts
+++ api.ts
@@ -1,7 +1,13 @@
 import { Router } from "http://deno.land/x/oak@v5.0.0/mod.ts";
+import * as planets from "./models/planets.ts";
+
 const router = new Router();
 
 router.get("/", (ctx) => {
   ctx.response.body = "Mission Control API";
+});
+
+router.get("/planets", (ctx) => {
+  ctx.response.body = planets.getAllPlanets();
 });
 export default router;
